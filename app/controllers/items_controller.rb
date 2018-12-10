b40cb2e532be2a0f3271def584e0219ba77e0f47
Kevin Peery <kevin.peery@gmail.com>
2018-12-10 15:53:39 -0700

    update departments and items controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,34 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :update, :destroy]
-  
+  before_action :set_department
+
   def index
-    render json: Item.all
+    item = @department.items.all
   end
 
   def show
+    render json: @item
+  end
+
+  def create
+    item = @department.item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: item.errors, status: 422
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      render json: item
+    else
+      render json: item.errors, status: 422
+    end
+  end
+
+  def destroy
+    @item.destroy
   end
 
   private
@@ -14,7 +37,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def set_department
+    @department = Department.find(params[:department_id])
+  end
+
   def item_params
-    params.require(:item).permit(:name, :price)
+    params.require(:item).permit(:name, :description, :price)
   end
 end
