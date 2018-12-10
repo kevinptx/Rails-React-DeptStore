8fa3bee6696e96982a1e6f2eb39e2375363760a7
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-12-10 13:51:04 -0700

    init commit

---
+++ db/schema.rb
@@ -0,0 +1,22 @@
+ActiveRecord::Schema.define(version: 2018_12_10_204511) do
+
+  enable_extension "plpgsql"
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.bigint "department_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["department_id"], name: "index_products_on_department_id"
+  end
+
+  add_foreign_key "products", "departments"
+end
